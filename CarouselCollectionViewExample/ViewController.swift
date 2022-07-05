1e7810e2dd8bd4ef1bdd738bb2fdc547b920afa6
Yoonjiwon <k112751@koreainvestment.com>
2022-07-05 09:54:20 +0900

    Create CarouselCollectionVIew
    Set a default skeleton of CarouselCollectionView, add CollectionView for the Carousel

--- CarouselCollectionViewExample/ViewController.swift
+++ CarouselCollectionViewExample/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+
+        let collectionView = UICollectionView(
+            frame: .zero,
+            collectionViewLayout: layout
+        )
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        numberOfItemsInSection section: Int
+    ) -> Int {
+        return 5
     }
 
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "cell",
+            for: indexPath
+        )
 
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+}
+
+private extension ViewController {
+    func setupViews() {
+        [
+            collectionView
+        ]
+            .forEach {
+                view.addSubview($0)
+            }
+    }
 }
 
