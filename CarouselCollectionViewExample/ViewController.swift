4f5738bbd28d5b998bde2cb4aa868c75a1517916
Yoonjiwon <k112751@koreainvestment.com>
2022-07-05 10:17:19 +0900

    Apply SnapKit and Set UI
    Make a collectionView layout, set default UICollectionViewCell with backgroundColor

--- CarouselCollectionViewExample/ViewController.swift
+++ CarouselCollectionViewExample/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     private lazy var collectionView: UICollectionView = {
@@ -19,11 +20,15 @@ class ViewController: UIViewController {
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.showsHorizontalScrollIndicator = false
         return collectionView
     }()
 
+    private let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue]
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
     }
 }
 
@@ -32,7 +37,7 @@ extension ViewController: UICollectionViewDataSource {
         _ collectionView: UICollectionView,
         numberOfItemsInSection section: Int
     ) -> Int {
-        return 5
+        return colors.count
     }
 
     func collectionView(
@@ -44,12 +49,22 @@ extension ViewController: UICollectionViewDataSource {
             for: indexPath
         )
 
+        cell.backgroundColor = colors[indexPath.row]
         return cell
     }
 }
 
 extension ViewController: UICollectionViewDelegateFlowLayout {
-
+    func collectionView(
+        _ collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAt indexPath: IndexPath
+    ) -> CGSize {
+        return CGSize(
+            width: UIScreen.main.bounds.width,
+            height: 300.0
+        )
+    }
 }
 
 private extension ViewController {
@@ -60,6 +75,14 @@ private extension ViewController {
             .forEach {
                 view.addSubview($0)
             }
+
+        collectionView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16.0)
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(300.0)
+        }
+
     }
 }
 
